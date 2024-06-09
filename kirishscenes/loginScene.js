816ddc305095e16d97b17b2a0f167c6dd4a6cff0
Master-Con <156166065+Master-Con@users.noreply.github.com>
2024-06-09 18:20:54 +0500

    Update loginScene.js

--- kirishscenes/loginScene.js
+++ kirishscenes/loginScene.js
@@ -2,11 +2,21 @@ const Scene = require('telegraf/scenes/base')
 
 const loginScene = new Scene('loginScene')
 
-loginScene.enter(ctx => ctx.reply('🔒Loginni kiriting:'))
+loginScene.enter(ctx => ctx.reply('🔒Loginni kiriting:', {
+    reply_markup: {
+        inline_keyboard: [
+            [{ text: `🔙Orqaga`, callback_data: `back_loginScene_uz`}]
+        ]
+    }
+}))
 
 loginScene.on('text', async ctx => {
     ctx.session.login = ctx.message.text;
     return ctx.scene.enter('passwordScene');
 });
 
+loginScene.action(`back_loginScene_uz`, async ctx => {
+    await ctx.deleteMessage();
+    ctx.scene.leave()
+})
 module.exports = loginScene
