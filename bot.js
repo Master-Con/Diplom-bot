fdda07514b470a01d55cec0e2bf79b461fe9b876
Master-Con <156166065+Master-Con@users.noreply.github.com>
2024-06-11 14:24:55 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const Stage = require('telegraf/stage')
 const Scene = require('telegraf/scenes/base')
 const mongoose = require('mongoose');
 const User = require('./models/User')
-mongoose.connect('mongodb+srv://winter:<MbDvSaLO76HN6utj>@cluster0.ymfagca.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/lowara');
+mongoose.connect('mongodb+srv://winter:MbDvSaLO76HN6utj@cluster0.ymfagca.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/lowara');
 
 
 const bot = new Telegraf(process.env.Token);
